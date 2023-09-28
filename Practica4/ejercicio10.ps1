5a2e36a9fae136b3fa3fa0a20b068ce608e1e2b1
Jesús <jesusvg97cs@gmail.com>
2023-09-28 09:29:22 +0200

    extras

--- Practica4/ejercicio10.ps1
+++ Practica4/ejercicio10.ps1
@@ -2,40 +2,40 @@
 do {
     Clear-Host
     Write-Host "Menú de opciones:"
-    Write-Host "1) Crear una carpeta"
-    Write-Host "2) Crear un fichero nuevo"
-    Write-Host "3) Cambiar el nombre de un fichero o carpeta"
-    Write-Host "4) Borrar un archivo o carpeta"
-    Write-Host "5) Verificar si existe un fichero o carpeta"
-    Write-Host "6) Mostrar el contenido de un directorio"
-    Write-Host "7) Mostrar la fecha y hora actuales"
-    Write-Host "8) Salir"
+    Write-Host "a) Crear una carpeta"
+    Write-Host "b) Crear un fichero nuevo"
+    Write-Host "c) Cambiar el nombre de un fichero o carpeta"
+    Write-Host "d) Borrar un archivo o carpeta"
+    Write-Host "e) Verificar si existe un fichero o carpeta"
+    Write-Host "f) Mostrar el contenido de un directorio"
+    Write-Host "g) Mostrar la fecha y hora actuales"
+    Write-Host "x) Salir"
 
-    $opcion = Read-Host "Elige una opción"
+   [string] $opcion = Read-Host "Elige una opción"
 
     switch ($opcion) {
-        1 { 
+        "a" { 
         $nombreCarpeta = Read-Host "Introduce el nombre de la carpeta a crear"
         New-Item -ItemType Directory -Path $nombreCarpeta
         Write-Host "Carpeta creada: $nombreCarpeta" 
         }
-        2 { 
+        "b" { 
             $nombreFichero = Read-Host "Introduce el nombre del fichero a crear"
             New-Item -ItemType File -Path $nombreFichero
             Write-Host "Fichero creado: $nombreFichero" 
           }
-        3 { 
+        "c" { 
             $nombreAnterior = Read-Host "Introduce el nombre actual del fichero o carpeta"
             $nombreNuevo = Read-Host "Introduce el nuevo nombre"
             Rename-Item -Path $nombreAnterior -NewName $nombreNuevo
             Write-Host "Nombre cambiado a: $nombreNuevo" 
           }
-        4 { 
+        "d" { 
             $path = Read-Host "Introduce la ruta del archivo o carpeta a borrar"
             Remove-Item -Path $path -Force
             Write-Host "Archivo o carpeta eliminado: $path" 
         }
-        5 {  
+        "e" {  
             $path = Read-Host "Introduce la ruta del archivo o carpeta a verificar"
             $existe = Test-Path $path
             if ($existe) {
@@ -44,16 +44,16 @@ do {
                 Write-Host "El archivo o carpeta $path no existe."
             } 
           }
-        6 { 
+        "f" { 
             $rutaDirectorio = Read-Host "Introduce la ruta del directorio"
             Get-ChildItem -Path $rutaDirectorio  
           }
-        7 { Get-Date
+        "g" { Get-Date
          
           }
 
-        8 { 
-            break 
+        "x" { 
+            exit 
           }
         default { Write-Host "Opción no válida. Inténtalo de nuevo." }
     }
