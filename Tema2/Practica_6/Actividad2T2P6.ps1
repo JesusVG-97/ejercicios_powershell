daa7c8d3e68230db67e395ea3d1d32bc90ae4178
Jesús <jesusvg97cs@gmail.com>
2023-10-24 13:38:01 +0200

    Primera parte del script

---
+++ Tema2/Practica_6/Actividad2T2P6.ps1
@@ -0,0 +1,23 @@
+﻿Import-Module ActiveDirectory
+
+# Crear la carpeta principal y compartirla de forma oculta
+New-Item -Path "C:\Empresa.users" -ItemType Directory
+New-SmbShare -Path "C:\Empresa.users" -Name "Empresa.users$" -Description "Carpeta compartida oculta para usuarios" -ScopeName "*" -FullAccess "Administradores"
+
+$empleados = Import-Csv "C:\Users\Administrador\Desktop\empleados.csv" -Delimiter ";"
+
+foreach ($e in $empleados) {
+    $nombre = $e.nombre
+    $apellido = $e.apellido
+    $usuario = "$nombre.$apellido"
+    $ruta = "C:\Empresa.users\$usuario"
+
+    # Crear la carpeta personal del usuario
+    New-Item -Path $ruta -ItemType Directory
+
+    # Compartir la carpeta personal con Control Total para el usuario propietario
+    New-SmbShare -Path $ruta -Name "$usuario$" -Description "Carpeta personal de $usuario" -ScopeName "*" -FullAccess $usuario
+}
+
+# Revocar el acceso a "Todos" en la carpeta principal "Empresa.users"
+Revoke-SmbShareAccess -Name "Empresa.users$" -AccountName "Todos" -Force
